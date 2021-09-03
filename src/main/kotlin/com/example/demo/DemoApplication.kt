4b44d02bc7149f3b1147c5d4e9c3ba700eabaac9
aleksandr <Aleksandr.A.Tikhonov@rt.ru>
2021-09-03 23:04:23 +0300

    second commit

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -5,7 +5,10 @@ import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class DemoApplication
-
+//123
+//change 1
+//change 2
+//change 3
 fun main(args: Array<String>) {
 	runApplication<DemoApplication>(*args)
 }
